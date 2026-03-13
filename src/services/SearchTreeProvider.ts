0c03bf859bf8bae17ef4712ce4c6cd32fb01ca51
Daniel Ogbuike <ogbuikedaniel@gmail.com>
2026-03-13 21:25:44 +0100

    Tree Data Provider

---
+++ src/services/SearchTreeProvider.ts
@@ -0,0 +1,50 @@
+import * as vscode from "vscode";
+import type { IndexEntry } from "../types";
+
+class SearchResultItem extends vscode.TreeItem {
+  constructor(entry: IndexEntry) {
+    super(
+      vscode.workspace.asRelativePath(entry.filePath),
+      vscode.TreeItemCollapsibleState.None,
+    );
+    this.description = entry.chunk.slice(0, 80);
+    this.iconPath = new vscode.ThemeIcon("file");
+    this.tooltip = entry.chunk;
+    this.command = {
+      command: "vscode.open",
+      title: "Open File",
+      arguments: [vscode.Uri.file(entry.filePath)],
+    };
+  }
+}
+
+export class SearchTreeProvider implements vscode.TreeDataProvider<SearchResultItem> {
+  constructor() {}
+  private results: SearchResultItem[] = [];
+  private _onDidChangeTreeData = new vscode.EventEmitter<
+    SearchResultItem | undefined | void
+  >();
+  readonly onDidChangeTreeData = this._onDidChangeTreeData.event;
+
+  getTreeItem(
+    element: SearchResultItem,
+  ): vscode.TreeItem | Thenable<vscode.TreeItem> {
+    return element;
+  }
+
+  getChildren(
+    element?: SearchResultItem | undefined,
+  ): vscode.ProviderResult<SearchResultItem[]> {
+    return this.results;
+  }
+
+  updateResults(entries: IndexEntry[]) {
+    this.results = entries.map((entry) => new SearchResultItem(entry));
+    this._onDidChangeTreeData.fire();
+  }
+
+  clearResults() {
+    this.results = [];
+    this._onDidChangeTreeData.fire();
+  }
+}
